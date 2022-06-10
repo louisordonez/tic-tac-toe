48ea6b289e51a2e01bc3e07253e29dd195055b64
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-06-10 22:59:45 +0800

    Add winning combination logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,182 @@ const addMoveToList = (text) => {
   movesModalList.appendChild(newP)
 }
 
+// Wining Combination
+const xWinCombo = [
+  [
+    ['X', 'X', 'X'],
+    ['', '', ''],
+    ['', '', ''],
+  ],
+  [
+    ['', '', ''],
+    ['X', 'X', 'X'],
+    ['', '', ''],
+  ],
+  [
+    ['', '', ''],
+    ['', '', ''],
+    ['X', 'X', 'X'],
+  ],
+  [
+    ['X', '', ''],
+    ['X', '', ''],
+    ['X', '', ''],
+  ],
+  [
+    ['', 'X', ''],
+    ['', 'X', ''],
+    ['', 'X', ''],
+  ],
+  [
+    ['', '', 'X'],
+    ['', '', 'X'],
+    ['', '', 'X'],
+  ],
+  [
+    ['X', '', ''],
+    ['', 'X', ''],
+    ['', '', 'X'],
+  ],
+  [
+    ['', '', 'X'],
+    ['', 'X', ''],
+    ['X', '', ''],
+  ],
+]
+
+const oWinCombo = [
+  [
+    ['O', 'O', 'O'],
+    ['', '', ''],
+    ['', '', ''],
+  ],
+  [
+    ['', '', ''],
+    ['O', 'O', 'O'],
+    ['', '', ''],
+  ],
+  [
+    ['', '', ''],
+    ['', '', ''],
+    ['O', 'O', 'O'],
+  ],
+  [
+    ['O', '', ''],
+    ['O', '', ''],
+    ['O', '', ''],
+  ],
+  [
+    ['', 'O', ''],
+    ['', 'O', ''],
+    ['', 'O', ''],
+  ],
+  [
+    ['', '', 'O'],
+    ['', '', 'O'],
+    ['', '', 'O'],
+  ],
+  [
+    ['O', '', ''],
+    ['', 'O', ''],
+    ['', '', 'O'],
+  ],
+  [
+    ['', '', 'O'],
+    ['', 'O', ''],
+    ['O', '', ''],
+  ],
+]
+
+const checkScore = () => {
+  let combo
+  if (playerTurn === 'O') {
+    combo = xWinCombo
+  } else {
+    combo = oWinCombo
+  }
+  if (state[0][0][0] === combo[0][0][0] && state[0][0][1] === combo[0][0][1] && state[0][0][2] === combo[0][0][2]) {
+    console.log(`win`)
+    console.log(`xxx`)
+    console.log(`---`)
+    console.log(`---`)
+  } else if (
+    state[0][1][0] === combo[1][1][0] &&
+    state[0][1][1] === combo[1][1][1] &&
+    state[0][1][2] === combo[1][1][2]
+  ) {
+    console.log(`win`)
+    console.log(`---`)
+    console.log(`xxx`)
+    console.log(`---`)
+  } else if (
+    state[0][2][0] === combo[2][2][0] &&
+    state[0][2][1] === combo[2][2][1] &&
+    state[0][2][2] === combo[2][2][2]
+  ) {
+    console.log(`win`)
+    console.log(`---`)
+    console.log(`---`)
+    console.log(`xxx`)
+  } else if (
+    state[0][0][0] === combo[3][0][0] &&
+    state[0][1][0] === combo[3][1][0] &&
+    state[0][2][0] === combo[3][2][0]
+  ) {
+    console.log(`win`)
+    console.log(`x--`)
+    console.log(`x--`)
+    console.log(`x--`)
+  } else if (
+    state[0][0][1] === combo[4][0][1] &&
+    state[0][1][1] === combo[4][1][1] &&
+    state[0][2][1] === combo[4][2][1]
+  ) {
+    console.log(`win`)
+    console.log(`-x-`)
+    console.log(`-x-`)
+    console.log(`-x-`)
+  } else if (
+    state[0][0][2] === combo[5][0][2] &&
+    state[0][1][2] === combo[5][1][2] &&
+    state[0][2][2] === combo[5][2][2]
+  ) {
+    console.log(`win`)
+    console.log(`--x`)
+    console.log(`--x`)
+    console.log(`--x`)
+  } else if (
+    state[0][0][0] === combo[6][0][0] &&
+    state[0][1][1] === combo[6][1][1] &&
+    state[0][2][2] === combo[6][2][2]
+  ) {
+    console.log(`win`)
+    console.log(`x--`)
+    console.log(`-x-`)
+    console.log(`--x`)
+  } else if (
+    state[0][0][2] === combo[7][0][2] &&
+    state[0][1][1] === combo[7][1][1] &&
+    state[0][2][0] === combo[7][2][0]
+  ) {
+    console.log(`win`)
+    console.log(`--x`)
+    console.log(`-x-`)
+    console.log(`x--`)
+  } else if (moves.length === 9) {
+    const noMovesArr = [
+      [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ],
+    ]
+    if (state[0][0] !== noMovesArr[0][0] && state[0][1] !== noMovesArr[0][1] && state[0][2] !== noMovesArr[0][2]) {
+      console.log(`draw`)
+    }
+  }
+}
+
 // Events
 upperLeftBox.addEventListener('click', () => {
   if (upperLeftBox.textContent === '') {
@@ -75,6 +251,7 @@ upperLeftBox.addEventListener('click', () => {
     console.log(moves)
     setPlayerTurn()
     setPlayerTurnText()
+    checkScore()
   }
 })
 upperMiddleBox.addEventListener('click', () => {
@@ -86,6 +263,7 @@ upperMiddleBox.addEventListener('click', () => {
     console.log(moves)
     setPlayerTurn()
     setPlayerTurnText()
+    checkScore()
   }
 })
 upperRightBox.addEventListener('click', () => {
@@ -97,6 +275,7 @@ upperRightBox.addEventListener('click', () => {
     console.log(moves)
     setPlayerTurn()
     setPlayerTurnText()
+    checkScore()
   }
 })
 middleLeftBox.addEventListener('click', () => {
@@ -108,6 +287,7 @@ middleLeftBox.addEventListener('click', () => {
     console.log(moves)
     setPlayerTurn()
     setPlayerTurnText()
+    checkScore()
   }
 })
 middleMiddleBox.addEventListener('click', () => {
@@ -119,6 +299,7 @@ middleMiddleBox.addEventListener('click', () => {
     console.log(moves)
     setPlayerTurn()
     setPlayerTurnText()
+    checkScore()
   }
 })
 middleRightBox.addEventListener('click', () => {
@@ -130,6 +311,7 @@ middleRightBox.addEventListener('click', () => {
     console.log(moves)
     setPlayerTurn()
     setPlayerTurnText()
+    checkScore()
   }
 })
 lowerLeftBox.addEventListener('click', () => {
@@ -141,6 +323,7 @@ lowerLeftBox.addEventListener('click', () => {
     console.log(moves)
     setPlayerTurn()
     setPlayerTurnText()
+    checkScore()
   }
 })
 lowerMiddleBox.addEventListener('click', () => {
@@ -152,6 +335,7 @@ lowerMiddleBox.addEventListener('click', () => {
     console.log(moves)
     setPlayerTurn()
     setPlayerTurnText()
+    checkScore()
   }
 })
 lowerRightBox.addEventListener('click', () => {
@@ -163,5 +347,6 @@ lowerRightBox.addEventListener('click', () => {
     console.log(moves)
     setPlayerTurn()
     setPlayerTurnText()
+    checkScore()
   }
 })
