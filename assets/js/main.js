f95001a579f0e1ad92b60dfe7af8205c5a472adc
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-06-10 12:52:14 +0800

    Update moves modal design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // Player
-const playerText = document.querySelector('[data-player]')
+const playerTurnText = document.querySelector('[data-player]')
 
 // Board
 const upperLeftBox = document.querySelector('[data-upper-left]')
@@ -21,6 +21,7 @@ const movesButton = document.querySelector('[data-moves]')
 // Modal
 const movesModal = document.querySelector('[data-moves-modal]')
 const movesModalClose = document.querySelector('[data-moves-modal-close]')
+const movesModalList = document.querySelector('[data-moves-list-container]')
 
 movesButton.addEventListener('click', () => (movesModal.style.display = 'block'))
 movesModalClose.addEventListener('click', () => (movesModal.style.display = 'none'))
@@ -31,25 +32,55 @@ window.addEventListener('click', (event) => {
 })
 
 // Turn
-let playerTurn = 'X'
-playerText.textContent = playerTurn
+let state = [
+  [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ],
+]
 
-const setPlayerTurn = () => {
-  if (playerTurn == 'X') {
-    playerTurn = 'O'
-  } else {
-    playerTurn = 'X'
-  }
-  playerText.textContent = playerTurn
-  return playerTurn
+let moves = []
+
+// let playerTurn = 'X'
+
+// const setPlayerTurn = () => {
+//   if (playerTurn === 'X') {
+//     playerTurn = 'O'
+//   } else if (playerTurn === 'O') {
+//     playerTurn = 'X'
+//   }
+// }
+
+const addMove = (text) => {
+  const newP = document.createElement('p')
+  const newContent = document.createTextNode(text)
+  newP.style.fontWeight = '600'
+  newP.appendChild(newContent)
+  movesModalList.appendChild(newP)
 }
 
-upperLeftBox.addEventListener('click', () => (upperLeftBox.textContent = setPlayerTurn()))
-upperMiddleBox.addEventListener('click', () => (upperMiddleBox.textContent = setPlayerTurn()))
-upperRightBox.addEventListener('click', () => (upperRightBox.textContent = setPlayerTurn()))
-middleLeftBox.addEventListener('click', () => (middleLeftBox.textContent = setPlayerTurn()))
-middleMiddleBox.addEventListener('click', () => (middleMiddleBox.textContent = setPlayerTurn()))
-middleRightBox.addEventListener('click', () => (middleRightBox.textContent = setPlayerTurn()))
-lowerLeftBox.addEventListener('click', () => (lowerLeftBox.textContent = setPlayerTurn()))
-lowerMiddleBox.addEventListener('click', () => (lowerMiddleBox.textContent = setPlayerTurn()))
-lowerRightBox.addEventListener('click', () => (lowerRightBox.textContent = setPlayerTurn()))
+upperLeftBox.addEventListener('click', () => {
+  if (upperLeftBox.textContent === '') {
+    state[0][0][0] = 'X'
+    upperLeftBox.textContent = 'X'
+    moves.push(JSON.parse(JSON.stringify(state)))
+    console.log(moves)
+  }
+})
+
+upperMiddleBox.addEventListener('click', () => {
+  if (upperMiddleBox.textContent === '') {
+    state[0][0][1] = 'O'
+    upperMiddleBox.textContent = 'O'
+    moves.push(JSON.parse(JSON.stringify(state)))
+    console.log(moves)
+  }
+})
+// upperRightBox.addEventListener('click', () => (upperRightBox.textContent = setPlayerTurn()))
+// middleLeftBox.addEventListener('click', () => (middleLeftBox.textContent = setPlayerTurn()))
+// middleMiddleBox.addEventListener('click', () => (middleMiddleBox.textContent = setPlayerTurn()))
+// middleRightBox.addEventListener('click', () => (middleRightBox.textContent = setPlayerTurn()))
+// lowerLeftBox.addEventListener('click', () => (lowerLeftBox.textContent = setPlayerTurn()))
+// lowerMiddleBox.addEventListener('click', () => (lowerMiddleBox.textContent = setPlayerTurn()))
+// lowerRightBox.addEventListener('click', () => (lowerRightBox.textContent = setPlayerTurn()))
