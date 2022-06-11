c76065f731eaf97c22a8bcf97aa77c7f2ae29726
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-06-11 20:58:53 +0800

    Add reset button

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -9,3 +9,43 @@ const enableButtons = () => {
 }
 
 enableButtons()
+
+// Moves
+const addMoveToList = (text) => {
+  const newP = document.createElement('p')
+  const newContent = document.createTextNode(text)
+  newP.style.fontWeight = '600'
+  newP.appendChild(newContent)
+  movesModalList.appendChild(newP)
+}
+
+movesButton.addEventListener('click', () => (movesModal.style.display = 'block'))
+movesModalClose.addEventListener('click', () => (movesModal.style.display = 'none'))
+window.addEventListener('click', (event) => {
+  if (event.target == movesModal) {
+    movesModal.style.display = 'none'
+  }
+})
+
+// Reset
+const resetGame = () => {
+  const box = document.getElementsByClassName('box')
+
+  done = false
+  moves.length = 0
+  state.forEach((rows) => {
+    rows.forEach((innerRows) => {
+      for (let i = 0; i < innerRows.length; i++) {
+        innerRows[i] = ''
+      }
+    })
+  })
+  for (let i = 0; i < box.length; i++) {
+    box[i].textContent = ''
+  }
+  playerTurn = 'X'
+  playerTurnText.textContent = `Turn: Player ${playerTurn}`
+  enableButtons()
+}
+
+resetButton.addEventListener('click', () => resetGame())
