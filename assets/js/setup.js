dcea2a7efdeff8ae771afe7b94fa1019780cc1f7
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-06-12 19:10:42 +0800

    Add handler for player name input

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -1,6 +1,15 @@
 const setPlayerName = () => {
   playerCrossName = playerCrossNameInput.value
   playerCircleName = playerCircleNameInput.value
+
+  if (playerCrossName === '' && playerCircleName !== '') {
+    playerCrossName = 'Player X'
+  } else if (playerCrossName !== '' && playerCircleName === '') {
+    playerCircleName = 'Player O'
+  } else if (playerCrossName === '' && playerCircleName === '') {
+    playerCrossName = 'Player X'
+    playerCircleName = 'Player O'
+  }
   showScore()
 }
 
